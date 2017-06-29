ad6ec613c414c38c8da041d8acf5a66e2a47f262
413112417 <413112417@qq.com>
2017-06-29 18:05:22 +0800

    Initial commit

---
+++ app/src/main/java/org/xjh/testtool/ui/CrashInfoActivity.java
@@ -0,0 +1,51 @@
+package org.xjh.testtool.ui;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import pers.xjh.test.R;
+import org.xjh.testtool.ui.base.BaseActivity;
+
+/**
+ * Created by XJH on 2017/4/30.
+ */
+
+public class CrashInfoActivity extends BaseActivity {
+
+    private TextView mTvCrashInfo;
+
+    private String mInfo;
+
+    @Override
+    protected int initContentView() {
+        return R.layout.test_activity_crash_info;
+    }
+
+    /**
+     * 初始化标题
+     */
+    @Override
+    protected void initTitle() {
+        super.initTitle();
+        mTitleBar.setTitle("崩溃详情");
+        mTitleBar.setLeftTitleClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    /**
+     * 初始化
+     */
+    @Override
+    protected void initView() {
+        Intent intent = getIntent();
+        if(intent != null) {
+            mInfo = intent.getStringExtra("crashInfo");
+        }
+        mTvCrashInfo = (TextView) findViewById(R.id.tv_crash_info);
+        mTvCrashInfo.setText(mInfo);
+    }
+}
